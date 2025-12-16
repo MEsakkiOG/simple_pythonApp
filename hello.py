1fd0ad3a97dd933b1530923a0b309e2607172867
velmurugan <ce.24503726@gmail.com>
2025-12-16 12:06:10 +0530

    Commited On 16-12-25/12:06 By Esakki

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 def greet():
-    print("Hello💻, welcome to GitHub Python app!")
+    print("Hello💻, welcome to GitHub Python app!\n")
+    print("User:Esakki")
 
 if __name__ == "__main__":
     greet()
